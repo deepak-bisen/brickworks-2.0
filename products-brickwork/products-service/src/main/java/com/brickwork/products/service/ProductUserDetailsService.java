2c72b9c9d0822ac38250e368a672cd1528c80589
Deepak14-12 <bisen1412@gmail.com>
2026-04-21 19:59:09 +0530

    completed products service

--- products-brickwork/products-service/src/main/java/com/brickwork/products/service/ProductUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.brickwork.products.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface ProductUserDetailsService {
-    public UserDetails loadUserByUsername(String username);
-}
