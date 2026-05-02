1f0f9fe6e263d426d05355c3cd3cb6cd52831e55
Deepak14-12 <bisen1412@gmail.com>
2026-05-02 21:15:25 +0530

    completed user service

--- users-brickwork/users-app/src/main/java/com/brickwork/users/UsersBrickworkApplication.java
+++ users-brickwork/users-app/src/main/java/com/brickwork/users/UsersBrickworkApplication.java
@@ -2,12 +2,22 @@ package com.brickwork.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class UsersBrickworkApplication
-{
-    public static void main( String[] args )
-    {
+@ComponentScan(basePackages = "com.brickwork.users")
+@RestController // <--- TEMPORARY ANNOTATION FOR HEALTH CHECK
+public class UsersBrickworkApplication {
+
+    public static void main(String[] args) {
         SpringApplication.run(UsersBrickworkApplication.class, args);
+    }
+
+    // <--- TEMPORARY PING METHOD FOR HEALTH CHECK--->
+    @GetMapping("/ping")
+    public String ping() {
+        return "Users Service is UP and responding!";
     }
 }
--- users-brickwork/users-service/src/main/java/com/brickwork/users/security/SecurityConfig.java
+++ users-brickwork/users-service/src/main/java/com/brickwork/users/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,7 +26,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // Open gates for login/register!
                         .anyRequest().authenticated()
