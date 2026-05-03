ef56c02b526c4d5851a2d4b2ddc6694d6793943e
Deepak14-12 <bisen1412@gmail.com>
2026-05-04 02:05:52 +0530

    started work on finance-brickwork

--- finance-brickwork/finance-app/src/main/java/com/brickwork/finance/FinanceBrickworkApplication.java
+++ finance-brickwork/finance-app/src/main/java/com/brickwork/finance/FinanceBrickworkApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.brickwork"})
+@ComponentScan(basePackages = {"com.brickwork"})
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.brickwork.finance.client")
 public class FinanceBrickworkApplication {
 
 	public static void main(String[] args) {
